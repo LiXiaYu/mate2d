c4d5b7dd310785e0c968303c0996be31dd47897d
Li XiaYu <liqixbd@hotmail.com>
2022-01-16 02:11:15 +0800

    小修

--- asteria.mate/test.cpp
+++ asteria.mate/test.cpp
@@ -8,6 +8,13 @@ auto bar = [&]() { return std::unique_ptr(inc) ;  };      // return by reference
 // `bar` refers to an "immutable variable" holding a function.
 // `bar()` refers to the same "variable" as `inc`.
 
-auto add(x) { return x + inc;  };   // return by value
+template<typename T>
+auto add(T x ) { return x + inc;  };   // return by value
 // `add` refers to an "immutable variable" holding a function.
 // `add(5)` refers to a "temporary" holding an `integer` of `47`.
+
+
+auto add(x1,x2,x3)
+{
+    return x1+x2+x3;
+};
